7d45afcb3b1889e175a323a1cc521f3535fa9d06
Jan11239 <161478522+Jan11239@users.noreply.github.com>
2024-10-31 14:08:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'matematica',
+    'qual o valor de PI?',
+    '3,14'
+)
+
+criaCartao(
+    'matematica',
+    'Oque significa quando o DELTA e maior que 0?',
+    'Significa que tera duas raizes distintas'
+)
+
+criaCartao(
+    'matematica',
+    'qual a raiz de 9?',
+    '3'
+)
